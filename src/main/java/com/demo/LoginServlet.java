80024a4ad716845416300a570ab9718957c573bf
Mrunali Deore <107094589+MrunaliDeore@users.noreply.github.com>
2022-08-23 22:56:07 +0530

    Refactor :  Name starts with Cap and has minimum 3 characters

---
+++ src/main/java/com/demo/LoginServlet.java
@@ -0,0 +1,37 @@
+package com.demo;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Pattern;
+
+@WebServlet( "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String user = request.getParameter("user");
+        String pwd = request.getParameter( "pwd");
+
+        String user = "[A-Z]{1}[a-z]{3,6}";
+        Pattern pattern = Pattern.compile(user);
+
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+
+        if (userID.equals(user) && password.equals(pwd)){
+            request.setAttribute("user",user);
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either use name or password is wrong.</font>");
+            rd.include(request,response);
+        }
+    }
+}
