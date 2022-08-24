ad39e3e279dd42d24c0ce09f24b577f54d40fba5
Mrunali Deore <107094589+MrunaliDeore@users.noreply.github.com>
2022-08-24 11:48:02 +0530

    Refactor : accept a valid Password

---
+++ src/main/java/com/demo/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.demo;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet(
+        description = "Login Servlet testing",
+        urlPatterns = { "/LoginServlet" },
+        initParams = { @WebInitParam(name="user",value="Mrunali"),
+                @WebInitParam(name="password",value="Mrunali@1112") }
+)
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = request.getParameter("user");
+        boolean validuser = (user != null) && user.matches("[A-Z]{1}[a-z]{3,6}");
+
+        String pwd = request.getParameter( "pwd");
+        boolean validpass = (pwd != null) && pwd.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
+
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+
+        if (userID.equals(user) && password.equals(pwd)){
+            request.setAttribute("user",user);
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either use name or password is wrong.</font>");
+            rd.include(request,response);
+        }
+    }
+}
